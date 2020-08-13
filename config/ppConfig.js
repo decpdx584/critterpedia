e8bce29959b31d96c4311a157104aecc90c3d662
General Assembly <generalassembly@Davids-MacBook-Pro.local>
2020-08-13 14:34:43 -0700

    [passport] Add ppConfig

---
+++ config/ppConfig.js
@@ -0,0 +1,32 @@
+const passport = require('passport');
+const localStrategy = require('passport-local').Strategy;
+const db = require('../models');
+
+// Passport "serializes" your info to make
+//it easier to log in
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+});
+// Passport "deserializedUser" is going to
+//take the id and look that up in the database
+passport.deserializedUser((id, cb) => {
+    cb(null, id)
+    .catch(cb());
+});
+
+passport.use(new localStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+}, (email, password, cb) => {
+    db.user.findOne( {
+        where: { email }
+    })
+    .then(user => {
+        if (!user || !user.validPassword(password)) {
+            cb(null, false);
+        } else {
+            cb(null, user);
+    }
+    })
+    .catch(cb());
+}));
